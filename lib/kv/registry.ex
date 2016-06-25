8caef200cb975cf80eae099fafc7e8fffc106a2d
Kuldeep Aggarwal <kd.engineer@yahoo.co.in>
2016-06-25 09:36:52 +0530

    complete addition of supervisor: Mix-OTP

---
+++ lib/kv/registry.ex
@@ -0,0 +1,67 @@
+defmodule KV.Registry do
+  use GenServer
+
+  ## Client API
+
+  @doc """
+  Starts a reigstry with the given `name`.
+  """
+  def start_link(name) do
+    GenServer.start_link __MODULE__, :ok, name: name
+  end
+
+  @doc """
+  Looks up the bucket pid for `name` stored in `server`.
+
+  Returns `{:ok, pid}` if the bucket exists, `:error` otherwise.
+  """
+  def lookup(server, name) do
+    GenServer.call(server, {:lookup, name})
+  end
+
+  @doc """
+  Stops the registry.
+  """
+  def stop(server) do
+    GenServer.stop(server)
+  end
+
+  ## Server API
+
+  @doc """
+  Ensures there is a bucket associated to the given `name` in `server`.
+  """
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
+
+  def init(:ok) do
+    {:ok, {%{}, %{}}}
+  end
+
+  def handle_call({:lookup, name}, _from, {names, _} = state) do
+    {:reply, Map.fetch(names, name), state}
+  end
+
+  def handle_cast({:create, name}, {names, refs}) do
+    if Map.has_key?(names, name) do
+      {:noreply, names}
+    else
+      {:ok, bucket} = KV.Bucket.Supervisor.start_bucket
+      ref = Process.monitor(bucket)
+      refs = Map.put(refs, ref, name)
+      names = Map.put(names, name, bucket)
+      {:noreply, {names, refs}}
+    end
+  end
+
+  def handle_info({:DOWN, ref, :process, _pid, _reason}, {names, refs}) do
+    {name, refs} = Map.pop(refs, ref)
+    names = Map.delete(names, name)
+    {:noreply, {names, refs}}
+  end
+
+  def handle_info(msg, state) do
+    super(msg, state)
+  end
+end
